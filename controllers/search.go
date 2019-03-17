80f3b922ec9425245069dfdcf43e105029f47c3b
rumblefrog <contact@zhenyangli.me>
2019-03-17 02:18:28 -0400

    refactor(crawler): refactor crawler into separate pkg

--- controllers/search.go
+++ controllers/search.go
@@ -1,14 +1,9 @@
 package controllers
 
 import (
-	"net/url"
-	"strconv"
-	"strings"
-
 	"github.com/gin-gonic/gin"
-	"github.com/gocolly/colly"
+	"github.com/rumblefrog/Youtube-WS/crawler"
 	"github.com/rumblefrog/Youtube-WS/models"
-	"github.com/rumblefrog/Youtube-WS/utils"
 )
 
 func Search(c *gin.Context) {
@@ -26,64 +21,11 @@ func Search(c *gin.Context) {
 		return
 	}
 
-	searchCollector := colly.NewCollector()
+	complete := make(chan *models.Result)
 
-	result := &models.Result{}
+	go crawler.Crawl(&q, complete)
 
-	searchCollector.OnHTML("li > div.yt-lockup > div.yt-lockup-dismissable", func(e *colly.HTMLElement) {
-		video := &models.Video{}
+	result := <-complete
 
-		video.VCode = strings.Split(e.ChildAttr("div.yt-lockup-thumbnail a.yt-uix-sessionlink", "href"), "?v=")[1]
-		video.Thumbnail = e.ChildAttr("div.yt-lockup-thumbnail span.yt-thumb-simple img", "src")
-
-		if video.Thumbnail[0:4] == "/yts" {
-			video.Thumbnail = e.ChildAttr("div.yt-lockup-thumbnail span.yt-thumb-simple img", "data-thumb")
-		}
-
-		video.Title = e.ChildAttr("h3.yt-lockup-title > a.yt-uix-tile-link", "title")
-		video.Creator = e.ChildText("div.yt-lockup-byline > a")
-		video.TimeLapsed = e.ChildText("div.yt-lockup-meta li:first-child")
-		video.Views = e.ChildText("div.yt-lockup-meta li:nth-child(2)")
-		video.Description = e.ChildText("div.yt-lockup-description")
-
-		result.Videos = append(result.Videos, video)
-	})
-
-	searchCollector.OnHTML("div.search-pager", func(e *colly.HTMLElement) {
-		result.CurrentPage, _ = strconv.ParseInt(e.ChildText("button.yt-uix-button > span.yt-uix-button-content"), 10, 64)
-
-		cp := &models.Pager{}
-
-		cp.Meta = utils.ParsePager(e.ChildAttr("button.yt-uix-button", "data-redirect-url"), true)
-		cp.Page, _ = strconv.ParseInt(e.ChildText("button.yt-uix-button > span.yt-uix-button-content"), 10, 64)
-
-		result.Pagination = append(result.Pagination, cp)
-
-		e.ForEach("a.yt-uix-button", func(i int, el *colly.HTMLElement) {
-			pager := &models.Pager{}
-
-			pager.Meta = utils.ParsePager(el.Attr("href"), false)
-			pager.Page, _ = strconv.ParseInt(el.ChildText("span.yt-uix-button-content"), 10, 64)
-
-			result.Pagination = append(result.Pagination, pager)
-		})
-	})
-
-	searchCollector.OnScraped(func(r *colly.Response) {
-		c.JSON(200, result)
-	})
-
-	yl, _ := url.Parse("https://www.youtube.com/results")
-
-	subQ := yl.Query()
-
-	subQ.Add("search_query", q.Keyword)
-
-	if q.SP != "" {
-		subQ.Add("sp", q.SP)
-	}
-
-	yl.RawQuery = subQ.Encode()
-
-	searchCollector.Visit(yl.String())
+	c.JSON(200, result)
 }
---
+++ crawler/crawler.go
@@ -0,0 +1,78 @@
+package crawler
+
+import (
+	"log"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/gocolly/colly"
+	"github.com/rumblefrog/Youtube-WS/models"
+	"github.com/rumblefrog/Youtube-WS/utils"
+)
+
+func Crawl(q *models.Query, complete chan<- *models.Result) {
+	searchCollector := colly.NewCollector()
+
+	result := &models.Result{}
+
+	searchCollector.OnHTML("li > div.yt-lockup > div.yt-lockup-dismissable", func(e *colly.HTMLElement) {
+		video := &models.Video{}
+
+		video.VCode = strings.Split(e.ChildAttr("div.yt-lockup-thumbnail a.yt-uix-sessionlink", "href"), "?v=")[1]
+		video.Thumbnail = e.ChildAttr("div.yt-lockup-thumbnail span.yt-thumb-simple img", "src")
+
+		if video.Thumbnail[0:4] == "/yts" {
+			video.Thumbnail = e.ChildAttr("div.yt-lockup-thumbnail span.yt-thumb-simple img", "data-thumb")
+		}
+
+		video.Title = e.ChildAttr("h3.yt-lockup-title > a.yt-uix-tile-link", "title")
+		video.Creator = e.ChildText("div.yt-lockup-byline > a")
+		video.TimeLapsed = e.ChildText("div.yt-lockup-meta li:first-child")
+		video.Views = e.ChildText("div.yt-lockup-meta li:nth-child(2)")
+		video.Description = e.ChildText("div.yt-lockup-description")
+
+		result.Videos = append(result.Videos, video)
+	})
+
+	searchCollector.OnHTML("div.search-pager", func(e *colly.HTMLElement) {
+		result.CurrentPage, _ = strconv.ParseInt(e.ChildText("button.yt-uix-button > span.yt-uix-button-content"), 10, 64)
+
+		cp := &models.Pager{}
+
+		cp.Meta = utils.ParsePager(e.ChildAttr("button.yt-uix-button", "data-redirect-url"), true)
+		cp.Page, _ = strconv.ParseInt(e.ChildText("button.yt-uix-button > span.yt-uix-button-content"), 10, 64)
+
+		result.Pagination = append(result.Pagination, cp)
+
+		e.ForEach("a.yt-uix-button", func(i int, el *colly.HTMLElement) {
+			pager := &models.Pager{}
+
+			pager.Meta = utils.ParsePager(el.Attr("href"), false)
+			pager.Page, _ = strconv.ParseInt(el.ChildText("span.yt-uix-button-content"), 10, 64)
+
+			result.Pagination = append(result.Pagination, pager)
+		})
+	})
+
+	searchCollector.OnScraped(func(r *colly.Response) {
+		complete <- result
+		log.Println("Done")
+	})
+
+	yl, _ := url.Parse("https://www.youtube.com/results")
+
+	subQ := yl.Query()
+
+	subQ.Add("search_query", q.Keyword)
+
+	if q.SP != "" {
+		subQ.Add("sp", q.SP)
+	}
+
+	yl.RawQuery = subQ.Encode()
+
+	searchCollector.Visit(yl.String())
+
+	return
+}
